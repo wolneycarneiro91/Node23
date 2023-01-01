25b9bab2f5a522873992a73cdf673b4bf90afa9d
wolneycarneiro91 <46429367+wolneycarneiro91@users.noreply.github.com>
2023-01-01 16:00:43 -0300

    chore Configuration

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import express from "express";
+import { config } from "dotenv";
+config();
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+app.listen(port, () => console.log(`listen ${port}`));
